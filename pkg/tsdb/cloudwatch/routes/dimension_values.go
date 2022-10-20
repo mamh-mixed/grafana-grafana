3f2cb6c870b49a3f4ad4de5915a93eea436ed099
Erik Sundell <erik.sundell87@gmail.com>
2022-10-20 13:47:09 +0200

    add tests

--- pkg/tsdb/cloudwatch/routes/dimension_values.go
+++ pkg/tsdb/cloudwatch/routes/dimension_values.go
@@ -13,22 +13,22 @@ import (
 func DimensionValuesHandler(pluginCtx backend.PluginContext, clientFactory models.ClientsFactoryFunc, parameters url.Values) ([]byte, *models.HttpError) {
 	dimensionValuesRequest, err := request.GetDimensionValuesRequest(parameters)
 	if err != nil {
-		return nil, models.NewHttpError("error in DimensionKeyHandler", http.StatusBadRequest, err)
+		return nil, models.NewHttpError("error in DimensionValuesHandler", http.StatusBadRequest, err)
 	}
 
 	service, err := newListMetricsService(pluginCtx, clientFactory, dimensionValuesRequest.Region)
 	if err != nil {
-		return nil, models.NewHttpError("error in DimensionKeyHandler", http.StatusInternalServerError, err)
+		return nil, models.NewHttpError("error in DimensionValuesHandler", http.StatusInternalServerError, err)
 	}
 
 	dimensionValues, err := service.GetDimensionValuesByDimensionFilter(dimensionValuesRequest)
 	if err != nil {
-		return nil, models.NewHttpError("error in DimensionKeyHandler", http.StatusInternalServerError, err)
+		return nil, models.NewHttpError("error in DimensionValuesHandler", http.StatusInternalServerError, err)
 	}
 
 	dimensionValuesResponse, err := json.Marshal(dimensionValues)
 	if err != nil {
-		return nil, models.NewHttpError("error in DimensionKeyHandler", http.StatusInternalServerError, err)
+		return nil, models.NewHttpError("error in DimensionValuesHandler", http.StatusInternalServerError, err)
 	}
 
 	return dimensionValuesResponse, nil
