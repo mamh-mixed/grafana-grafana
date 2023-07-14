bb27f4edafe6a72d534513bc76f462b52822a8d3
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-07-14 16:49:02 -0400

    add same to dataplane

--- pkg/expr/dataplane.go
+++ pkg/expr/dataplane.go
@@ -10,10 +10,11 @@ import (
 	"github.com/grafana/dataplane/sdata/reader"
 	"github.com/grafana/dataplane/sdata/timeseries"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
+	"go.opentelemetry.io/otel/attribute"
+
 	"github.com/grafana/grafana/pkg/expr/mathexp"
 	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/infra/tracing"
-	"go.opentelemetry.io/otel/attribute"
 )
 
 func shouldUseDataplane(frames data.Frames, logger log.Logger, disable bool) (dt data.FrameType, b bool, e error) {
@@ -94,8 +95,20 @@ func handleDataplaneTimeseries(frames data.Frames) (mathexp.Results, error) {
 	}
 	res := mathexp.Results{}
 	res.Values = make([]mathexp.Value, 0, len(sc.Refs))
+
+	addNameLabel := shouldUseLabelName(len(sc.Refs), func(i int) data.Labels {
+		return sc.Refs[i].GetLabels()
+	}, func(i int) string {
+		return sc.Refs[i].GetMetricName()
+	})
+
 	for _, s := range sc.Refs {
 		newSeries, err := mathexp.NewSeriesFromRef(sc.RefID, s)
+		if addNameLabel {
+			newSeries.SetLabels(data.Labels{
+				nameLabelName: s.GetMetricName(),
+			})
+		}
 		if err != nil {
 			return res, err
 		}
@@ -122,10 +135,22 @@ func handleDataplaneNumeric(frames data.Frames) (mathexp.Results, error) {
 		}
 		return mathexp.Results{Values: mathexp.Values{noData}}, nil
 	}
+
+	addNameLabel := shouldUseLabelName(len(nc.Refs), func(i int) data.Labels {
+		return nc.Refs[i].GetLabels()
+	}, func(i int) string {
+		return nc.Refs[i].GetMetricName()
+	})
+
 	res := mathexp.Results{}
 	res.Values = make([]mathexp.Value, 0, len(nc.Refs))
 	for _, n := range nc.Refs {
 		newNum, err := mathexp.NumberFromRef(nc.RefID, n)
+		if addNameLabel {
+			newNum.SetLabels(data.Labels{
+				nameLabelName: n.GetMetricName(),
+			})
+		}
 		if err != nil {
 			return res, err
 		}
@@ -133,4 +158,22 @@ func handleDataplaneNumeric(frames data.Frames) (mathexp.Results, error) {
 	}
 
 	return res, nil
+}
+
+func shouldUseLabelName(count int, getLabels func(int) data.Labels, getName func(int) string) bool {
+	for i := 0; i < count; i++ {
+		if len(getLabels(i)) > 0 {
+			return false
+		}
+	}
+
+	names := make(map[string]struct{}, count)
+	for i := 0; i < count; i++ {
+		name := getName(i)
+		if _, ok := names[name]; ok {
+			return false
+		}
+		names[name] = struct{}{}
+	}
+	return true
 }
