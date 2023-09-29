50cffb856f02e86de51dd9a4e629b9aac84bbb7c
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-09-29 11:25:09 -0400

    add compat layer
    to convert from Export model to "new" API models

--- pkg/services/ngalert/api/compat.go
+++ pkg/services/ngalert/api/compat.go
@@ -2,12 +2,21 @@ package api
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
+	"unsafe"
 
+	"github.com/grafana/alerting/notify"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/modern-go/reflect2"
 	"github.com/prometheus/common/model"
 
 	"github.com/grafana/grafana/pkg/services/ngalert/api/tooling/definitions"
 	"github.com/grafana/grafana/pkg/services/ngalert/models"
+	"github.com/grafana/grafana/pkg/util"
 )
 
 // AlertRuleFromProvisionedAlertRule converts definitions.ProvisionedAlertRule to models.AlertRule
@@ -319,4 +328,398 @@ func RouteExportFromRoute(route *definitions.Route) *definitions.RouteExport {
 	}
 
 	return &export
+}
+
+func ContactPointFromContactPointExport(rawContactPoint definitions.ContactPointExport) (definitions.ContactPoint, error) {
+	j := jsoniter.ConfigCompatibleWithStandardLibrary
+	j.RegisterExtension(&contactPointsExtension{})
+
+	contactPoint := definitions.ContactPoint{
+		Name: rawContactPoint.Name,
+	}
+	var errs []error
+	for _, rawIntegration := range rawContactPoint.Receivers {
+		err := parseIntegration(j, &contactPoint, rawIntegration.Type, rawIntegration.DisableResolveMessage, json.RawMessage(rawIntegration.Settings))
+		if err != nil {
+			// accumulate errors to report all at once.
+			errs = append(errs, fmt.Errorf("failed to parse %s integration (uid:%s): %w", rawIntegration.Type, rawIntegration.UID, err))
+		}
+	}
+	return contactPoint, errors.Join(errs...)
+}
+
+// ContactPointToContactPointExport converts definitions.ContactPoint to notify.APIReceiver.
+// It uses special extension for jsoniter.API that properly handles marshalling of some specific fields
+//nolint:gocyclo
+func ContactPointToContactPointExport(cp definitions.ContactPoint) (notify.APIReceiver, error) {
+	j := jsoniter.ConfigCompatibleWithStandardLibrary
+	j.RegisterExtension(&contactPointsExtension{})
+
+	var integration []*notify.GrafanaIntegrationConfig
+
+	var errs []error
+	for _, i := range cp.Alertmanager {
+		el, err := marshallIntegration(j, "prometheus-alertmanager", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Dingding {
+		el, err := marshallIntegration(j, "dingding", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Discord {
+		el, err := marshallIntegration(j, "discord", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Email {
+		el, err := marshallIntegration(j, "email", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Googlechat {
+		el, err := marshallIntegration(j, "googlechat", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Kafka {
+		el, err := marshallIntegration(j, "kafka", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Line {
+		el, err := marshallIntegration(j, "line", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Opsgenie {
+		el, err := marshallIntegration(j, "opsgenie", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Pagerduty {
+		el, err := marshallIntegration(j, "pagerduty", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.OnCall {
+		el, err := marshallIntegration(j, "oncall", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Pushover {
+		el, err := marshallIntegration(j, "pushover", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Sensugo {
+		el, err := marshallIntegration(j, "sensugo", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Slack {
+		el, err := marshallIntegration(j, "slack", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Teams {
+		el, err := marshallIntegration(j, "teams", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Telegram {
+		el, err := marshallIntegration(j, "telegram", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Threema {
+		el, err := marshallIntegration(j, "threema", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Victorops {
+		el, err := marshallIntegration(j, "victorops", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Webhook {
+		el, err := marshallIntegration(j, "webhook", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Wecom {
+		el, err := marshallIntegration(j, "wecom", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, i := range cp.Webex {
+		el, err := marshallIntegration(j, "webex", i, i.IntegrationBase)
+		integration = append(integration, el)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	if len(errs) > 0 {
+		return notify.APIReceiver{}, errors.Join(errs...)
+	}
+	contactPoint := notify.APIReceiver{
+		ConfigReceiver:      notify.ConfigReceiver{Name: cp.Name},
+		GrafanaIntegrations: notify.GrafanaIntegrations{Integrations: integration},
+	}
+	return contactPoint, nil
+}
+
+func marshallIntegration(json jsoniter.API, integrationType string, integration interface{}, base definitions.IntegrationBase) (*notify.GrafanaIntegrationConfig, error) {
+	data, err := json.Marshal(integration)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshall integration '%s' to JSON: %w", integrationType, err)
+	}
+	e := &notify.GrafanaIntegrationConfig{
+		Type:     integrationType,
+		Settings: data,
+	}
+	if base.DisableResolveMessage != nil {
+		e.DisableResolveMessage = *base.DisableResolveMessage
+	}
+	return e, nil
+}
+
+//nolint:gocyclo
+func parseIntegration(json jsoniter.API, result *definitions.ContactPoint, receiverType string, disableResolveMessage bool, data json.RawMessage) error {
+	base := definitions.IntegrationBase{}
+	if disableResolveMessage { // omit the value if false
+		base.DisableResolveMessage = util.Pointer(disableResolveMessage)
+	}
+	var err error
+	switch strings.ToLower(receiverType) {
+	case "prometheus-alertmanager":
+		integration := definitions.AlertmanagerIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Alertmanager = append(result.Alertmanager, integration)
+		}
+	case "dingding":
+		integration := definitions.DingdingIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Dingding = append(result.Dingding, integration)
+		}
+	case "discord":
+		integration := definitions.DiscordIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Discord = append(result.Discord, integration)
+		}
+	case "email":
+		integration := definitions.EmailIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Email = append(result.Email, integration)
+		}
+	case "googlechat":
+		integration := definitions.GooglechatIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Googlechat = append(result.Googlechat, integration)
+		}
+	case "kafka":
+		integration := definitions.KafkaIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Kafka = append(result.Kafka, integration)
+		}
+	case "line":
+		integration := definitions.LineIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Line = append(result.Line, integration)
+		}
+	case "opsgenie":
+		integration := definitions.OpsgenieIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Opsgenie = append(result.Opsgenie, integration)
+		}
+	case "pagerduty":
+		integration := definitions.PagerdutyIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Pagerduty = append(result.Pagerduty, integration)
+		}
+	case "oncall":
+		integration := definitions.OnCallIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.OnCall = append(result.OnCall, integration)
+		}
+	case "pushover":
+		integration := definitions.PushoverIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Pushover = append(result.Pushover, integration)
+		}
+	case "sensugo":
+		integration := definitions.SensugoIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Sensugo = append(result.Sensugo, integration)
+		}
+	case "slack":
+		integration := definitions.SlackIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Slack = append(result.Slack, integration)
+		}
+	case "teams":
+		integration := definitions.TeamsIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Teams = append(result.Teams, integration)
+		}
+	case "telegram":
+		integration := definitions.TelegramIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Telegram = append(result.Telegram, integration)
+		}
+	case "threema":
+		integration := definitions.ThreemaIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Threema = append(result.Threema, integration)
+		}
+	case "victorops":
+		integration := definitions.VictoropsIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Victorops = append(result.Victorops, integration)
+		}
+	case "webhook":
+		integration := definitions.WebhookIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Webhook = append(result.Webhook, integration)
+		}
+	case "wecom":
+		integration := definitions.WecomIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Wecom = append(result.Wecom, integration)
+		}
+	case "webex":
+		integration := definitions.WebexIntegration{IntegrationBase: base}
+		if err = json.Unmarshal(data, &integration); err == nil {
+			result.Webex = append(result.Webex, integration)
+		}
+	default:
+		err = fmt.Errorf("notifier %s is not supported", receiverType)
+	}
+	return err
+}
+
+// contactPointsExtension extends jsoniter with special decoders for some fields that are encoded differently in the legacy configuration.
+type contactPointsExtension struct {
+	jsoniter.DummyExtension
+}
+
+func (c contactPointsExtension) UpdateStructDescriptor(structDescriptor *jsoniter.StructDescriptor) {
+	if structDescriptor.Type == reflect2.TypeOf(definitions.EmailIntegration{}) {
+		bind := structDescriptor.GetField("Addresses")
+		codec := &emailAddressCodec{}
+		bind.Decoder = codec
+		bind.Encoder = codec
+	}
+	if structDescriptor.Type == reflect2.TypeOf(definitions.PushoverIntegration{}) {
+		codec := &numberAsStringCodec{}
+		for _, field := range []string{"AlertingPriority", "OKPriority", "Retry", "Expire"} {
+			desc := structDescriptor.GetField(field)
+			desc.Decoder = codec
+			desc.Encoder = codec
+		}
+	}
+	if structDescriptor.Type == reflect2.TypeOf(definitions.WebhookIntegration{}) {
+		codec := &numberAsStringCodec{}
+		desc := structDescriptor.GetField("MaxAlerts")
+		desc.Decoder = codec
+		desc.Encoder = codec
+	}
+}
+
+type emailAddressCodec struct{}
+
+func (d *emailAddressCodec) IsEmpty(ptr unsafe.Pointer) bool {
+	f := *(*[]string)(ptr)
+	return len(f) == 0
+}
+
+func (d *emailAddressCodec) Encode(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+	f := *(*[]string)(ptr)
+	addresses := strings.Join(f, ";")
+	stream.WriteString(addresses)
+}
+
+func (d *emailAddressCodec) Decode(ptr unsafe.Pointer, iter *jsoniter.Iterator) {
+	s := iter.ReadString()
+	emails := strings.FieldsFunc(strings.Trim(s, "\""), func(r rune) bool {
+		switch r {
+		case ',', ';', '\n':
+			return true
+		}
+		return false
+	})
+	*((*[]string)(ptr)) = emails
+}
+
+type numberAsStringCodec struct{}
+
+func (d *numberAsStringCodec) IsEmpty(ptr unsafe.Pointer) bool {
+	return *((*(*int))(ptr)) == nil
+}
+
+func (d *numberAsStringCodec) Encode(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+	val := *((*(*int))(ptr))
+	if val == nil {
+		stream.WriteNil()
+		return
+	}
+	stream.WriteInt(*val)
+}
+
+func (d *numberAsStringCodec) Decode(ptr unsafe.Pointer, iter *jsoniter.Iterator) {
+	valueType := iter.WhatIsNext()
+	var value int
+	switch valueType {
+	case jsoniter.NumberValue:
+		value = iter.ReadInt()
+	case jsoniter.StringValue:
+		str := iter.ReadString()
+		num, err := strconv.ParseInt(str, 10, 32)
+		if err != nil {
+			iter.ReportError("numberAsStringCodec", fmt.Sprintf("string does not represent an 32-bit integer number: %s", err.Error()))
+		}
+		value = int(num)
+	default:
+		iter.ReportError("numberAsStringCodec", "not number or string")
+	}
+	*((*(*int))(ptr)) = &value
 }
--- pkg/services/ngalert/api/compat_test.go
+++ pkg/services/ngalert/api/compat_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/grafana/pkg/services/ngalert/api/tooling/definitions"
+	"github.com/grafana/grafana/pkg/util"
 )
 
 func TestToModel(t *testing.T) {
@@ -34,4 +35,52 @@ func TestToModel(t *testing.T) {
 		require.NoError(t, err)
 		require.Len(t, tm.Rules, 1)
 	})
+}
+
+func TestContactPointFromContactPointExports(t *testing.T) {
+	cp := definitions.ContactPointExport{
+		OrgID: 1,
+		Name:  "Test",
+		Receivers: []definitions.ReceiverExport{
+			{
+				Type:                  "email",
+				Settings:              definitions.RawMessage(`{"addresses": "test@grafana.com,test2@grafana.com;test3@grafana.com\ntest4@granafa.com"}`),
+				DisableResolveMessage: false,
+			},
+			{
+				Type: "pushover",
+				Settings: definitions.RawMessage(`{
+				"priority": 1,
+				"okPriority": 2,
+				"retry": "555",
+				"expire": "333"
+			}`),
+				DisableResolveMessage: false,
+			},
+		},
+	}
+	res, err := ContactPointFromContactPointExports(cp)
+	require.NoError(t, err)
+	require.Len(t, res.Email, 1)
+	require.EqualValues(t, []string{
+		"test@grafana.com",
+		"test2@grafana.com",
+		"test3@grafana.com",
+		"test4@granafa.com",
+	}, res.Email[0].Addresses)
+	require.Len(t, res.Pushover, 1)
+	require.Equal(t, definitions.PushoverIntegration{
+		IntegrationBase:  definitions.IntegrationBase{},
+		UserKey:          "",
+		APIToken:         "",
+		AlertingPriority: util.Pointer(1),
+		OKPriority:       util.Pointer(2),
+		Retry:            util.Pointer(555),
+		Expire:           util.Pointer(333),
+		Device:           nil,
+		AlertingSound:    nil,
+		OKSound:          nil,
+		Title:            nil,
+		Message:          nil,
+	}, res.Pushover[0])
 }
