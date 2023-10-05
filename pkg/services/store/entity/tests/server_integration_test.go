vmignot/grafana

remove EntitySummary

pkg/services/store/entity/tests/server_integration_test.go
@@ -153,9 +153,11 @@ func TestIntegrationEntityServer(t *testing.T) {
 	t.Run("should be able to read persisted objects", func(t *testing.T) {
 		before := time.Now()
 		writeReq := &entity.WriteEntityRequest{
-			GRN:     grn,
-			Body:    body,
-			Comment: "first entity!",
+			Entity: &entity.Entity{
+				GRN:     grn,
+				Body:    body,
+				Message: "first entity!",
+			},
 		}
 		writeResp, err := testCtx.client.Write(ctx, writeReq)
 		require.NoError(t, err)
@@ -164,7 +166,7 @@ func TestIntegrationEntityServer(t *testing.T) {
 			updatedRange: []time.Time{before, time.Now()},
 			updatedBy:    fakeUser,
 			version:      &firstVersion,
-			comment:      &writeReq.Comment,
+			comment:      &writeReq.Entity.Message,
 		}
 		requireVersionMatch(t, writeResp.Entity, versionMatcher)
 
@@ -174,7 +176,6 @@ func TestIntegrationEntityServer(t *testing.T) {
 			WithBody: true,
 		})
 		require.NoError(t, err)
-		require.Nil(t, readResp.Summary)
 		require.NotNil(t, readResp)
 
 		foundGRN := readResp.GRN
@@ -218,9 +219,11 @@ func TestIntegrationEntityServer(t *testing.T) {
 		}
 
 		writeReq1 := &entity.WriteEntityRequest{
-			GRN:     grn,
-			Body:    body,
-			Comment: "first entity!",
+			Entity: &entity.Entity{
+				GRN:     grn,
+				Body:    body,
+				Message: "first entity!",
+			},
 		}
 		writeResp1, err := testCtx.client.Write(ctx, writeReq1)
 		require.NoError(t, err)
@@ -229,9 +232,11 @@ func TestIntegrationEntityServer(t *testing.T) {
 		body2 := []byte("{\"name\":\"John2\"}")
 
 		writeReq2 := &entity.WriteEntityRequest{
-			GRN:     grn,
-			Body:    body2,
-			Comment: "update1",
+			Entity: &entity.Entity{
+				GRN:     grn,
+				Body:    body2,
+				Message: "update1",
+			},
 		}
 		writeResp2, err := testCtx.client.Write(ctx, writeReq2)
 		require.NoError(t, err)
@@ -247,9 +252,11 @@ func TestIntegrationEntityServer(t *testing.T) {
 
 		body3 := []byte("{\"name\":\"John3\"}")
 		writeReq3 := &entity.WriteEntityRequest{
-			GRN:     grn,
-			Body:    body3,
-			Comment: "update3",
+			Entity: &entity.Entity{
+				GRN:     grn,
+				Body:    body3,
+				Message: "update3",
+			},
 		}
 		writeResp3, err := testCtx.client.Write(ctx, writeReq3)
 		require.NoError(t, err)
@@ -270,7 +277,6 @@ func TestIntegrationEntityServer(t *testing.T) {
 			WithBody: true,
 		})
 		require.NoError(t, err)
-		require.Nil(t, readRespLatest.Summary)
 		requireEntityMatch(t, readRespLatest, latestMatcher)
 
 		readRespFirstVer, err := testCtx.client.Read(ctx, &entity.ReadEntityRequest{
@@ -280,7 +286,6 @@ func TestIntegrationEntityServer(t *testing.T) {
 		})
 
 		require.NoError(t, err)
-		require.Nil(t, readRespFirstVer.Summary)
 		require.NotNil(t, readRespFirstVer)
 		requireEntityMatch(t, readRespFirstVer, rawEntityMatcher{
 			grn:          grn,
@@ -316,35 +321,43 @@ func TestIntegrationEntityServer(t *testing.T) {
 		uid4 := "uid4"
 		kind2 := entity.StandardKindPlaylist
 		w1, err := testCtx.client.Write(ctx, &entity.WriteEntityRequest{
-			GRN:  grn,
-			Body: body,
+			Entity: &entity.Entity{
+				GRN:  grn,
+				Body: body,
+			},
 		})
 		require.NoError(t, err)
 
 		w2, err := testCtx.client.Write(ctx, &entity.WriteEntityRequest{
-			GRN: &entity.GRN{
-				UID:  uid2,
-				Kind: kind,
+			Entity: &entity.Entity{
+				GRN: &entity.GRN{
+					UID:  uid2,
+					Kind: kind,
+				},
+				Body: body,
 			},
-			Body: body,
 		})
 		require.NoError(t, err)
 
 		w3, err := testCtx.client.Write(ctx, &entity.WriteEntityRequest{
-			GRN: &entity.GRN{
-				UID:  uid3,
-				Kind: kind2,
+			Entity: &entity.Entity{
+				GRN: &entity.GRN{
+					UID:  uid3,
+					Kind: kind2,
+				},
+				Body: body,
 			},
-			Body: body,
 		})
 		require.NoError(t, err)
 
 		w4, err := testCtx.client.Write(ctx, &entity.WriteEntityRequest{
-			GRN: &entity.GRN{
-				UID:  uid4,
-				Kind: kind2,
+			Entity: &entity.Entity{
+				GRN: &entity.GRN{
+					UID:  uid4,
+					Kind: kind2,
+				},
+				Body: body,
 			},
-			Body: body,
 		})
 		require.NoError(t, err)
 
@@ -396,20 +409,24 @@ func TestIntegrationEntityServer(t *testing.T) {
 	t.Run("should be able to filter objects based on their labels", func(t *testing.T) {
 		kind := entity.StandardKindDashboard
 		_, err := testCtx.client.Write(ctx, &entity.WriteEntityRequest{
-			GRN: &entity.GRN{
-				Kind: kind,
-				UID:  "blue-green",
+			Entity: &entity.Entity{
+				GRN: &entity.GRN{
+					Kind: kind,
+					UID:  "blue-green",
+				},
+				Body: []byte(dashboardWithTagsBlueGreen),
 			},
-			Body: []byte(dashboardWithTagsBlueGreen),
 		})
 		require.NoError(t, err)
 
 		_, err = testCtx.client.Write(ctx, &entity.WriteEntityRequest{
-			GRN: &entity.GRN{
-				Kind: kind,
-				UID:  "red-green",
+			Entity: &entity.Entity{
+				GRN: &entity.GRN{
+					Kind: kind,
+					UID:  "red-green",
+				},
+				Body: []byte(dashboardWithTagsRedGreen),
 			},
-			Body: []byte(dashboardWithTagsRedGreen),
 		})
 		require.NoError(t, err)
 

mamh-mixed/grafana-grafana

pkg/services/store/entity/tests/server_integration_test.go
@@ -154,9 +154,11 @@ func TestIntegrationEntityServer(t *testing.T) {
 	t.Run("should be able to read persisted objects", func(t *testing.T) {
 		before := time.Now()
 		writeReq := &entity.WriteEntityRequest{
-			GRN:     testGrn,
-			Body:    body,
-			Comment: "first entity!",
+			Entity: &entity.Entity{
+				GRN:     testGrn,
+				Body:    body,
+				Message: "first entity!",
+			},
 		}
 		writeResp, err := testCtx.client.Write(ctx, writeReq)
 		require.NoError(t, err)
@@ -165,7 +167,7 @@ func TestIntegrationEntityServer(t *testing.T) {
 			updatedRange: []time.Time{before, time.Now()},
 			updatedBy:    fakeUser,
 			version:      &firstVersion,
-			comment:      &writeReq.Comment,
+			comment:      &writeReq.Entity.Message,
 		}
 		requireVersionMatch(t, writeResp.Entity, versionMatcher)
 
@@ -175,7 +177,6 @@ func TestIntegrationEntityServer(t *testing.T) {
 			WithBody: true,
 		})
 		require.NoError(t, err)
-		require.Nil(t, readResp.Summary)
 		require.NotNil(t, readResp)
 
 		foundGRN := readResp.GRN
@@ -219,9 +220,11 @@ func TestIntegrationEntityServer(t *testing.T) {
 		}
 
 		writeReq1 := &entity.WriteEntityRequest{
-			GRN:     testGrn,
-			Body:    body,
-			Comment: "first entity!",
+			Entity: &entity.Entity{
+				GRN:     testGrn,
+				Body:    body,
+				Message: "first entity!",
+			},
 		}
 		writeResp1, err := testCtx.client.Write(ctx, writeReq1)
 		require.NoError(t, err)
@@ -230,9 +233,11 @@ func TestIntegrationEntityServer(t *testing.T) {
 		body2 := []byte("{\"name\":\"John2\"}")
 
 		writeReq2 := &entity.WriteEntityRequest{
-			GRN:     testGrn,
-			Body:    body2,
-			Comment: "update1",
+			Entity: &entity.Entity{
+				GRN:     testGrn,
+				Body:    body2,
+				Message: "update1",
+			},
 		}
 		writeResp2, err := testCtx.client.Write(ctx, writeReq2)
 		require.NoError(t, err)
@@ -248,9 +253,11 @@ func TestIntegrationEntityServer(t *testing.T) {
 
 		body3 := []byte("{\"name\":\"John3\"}")
 		writeReq3 := &entity.WriteEntityRequest{
-			GRN:     testGrn,
-			Body:    body3,
-			Comment: "update3",
+			Entity: &entity.Entity{
+				GRN:     testGrn,
+				Body:    body3,
+				Message: "update3",
+			},
 		}
 		writeResp3, err := testCtx.client.Write(ctx, writeReq3)
 		require.NoError(t, err)
@@ -271,7 +278,6 @@ func TestIntegrationEntityServer(t *testing.T) {
 			WithBody: true,
 		})
 		require.NoError(t, err)
-		require.Nil(t, readRespLatest.Summary)
 		requireEntityMatch(t, readRespLatest, latestMatcher)
 
 		readRespFirstVer, err := testCtx.client.Read(ctx, &entity.ReadEntityRequest{
@@ -281,7 +287,6 @@ func TestIntegrationEntityServer(t *testing.T) {
 		})
 
 		require.NoError(t, err)
-		require.Nil(t, readRespFirstVer.Summary)
 		require.NotNil(t, readRespFirstVer)
 		requireEntityMatch(t, readRespFirstVer, rawEntityMatcher{
 			grn:          testGrn,
@@ -317,35 +322,43 @@ func TestIntegrationEntityServer(t *testing.T) {
 		uid4 := "uid4"
 		kind2 := entity.StandardKindPlaylist
 		w1, err := testCtx.client.Write(ctx, &entity.WriteEntityRequest{
-			GRN:  testGrn,
-			Body: body,
+			Entity: &entity.Entity{
+				GRN:  testGrn,
+				Body: body,
+			},
 		})
 		require.NoError(t, err)
 
 		w2, err := testCtx.client.Write(ctx, &entity.WriteEntityRequest{
-			GRN: &grn.GRN{
-				ResourceIdentifier: uid2,
-				ResourceKind:       kind,
+			Entity: &entity.Entity{
+				GRN: &grn.GRN{
+					ResourceIdentifier: uid2,
+					ResourceKind:       kind,
+				},
+				Body: body,
 			},
-			Body: body,
 		})
 		require.NoError(t, err)
 
 		w3, err := testCtx.client.Write(ctx, &entity.WriteEntityRequest{
-			GRN: &grn.GRN{
-				ResourceIdentifier: uid3,
-				ResourceKind:       kind2,
+			Entity: &entity.Entity{
+				GRN: &grn.GRN{
+					ResourceIdentifier: uid3,
+					ResourceKind:       kind2,
+				},
+				Body: body,
 			},
-			Body: body,
 		})
 		require.NoError(t, err)
 
 		w4, err := testCtx.client.Write(ctx, &entity.WriteEntityRequest{
-			GRN: &grn.GRN{
-				ResourceIdentifier: uid4,
-				ResourceKind:       kind2,
+			Entity: &entity.Entity{
+				GRN: &grn.GRN{
+					ResourceIdentifier: uid4,
+					ResourceKind:       kind2,
+				},
+				Body: body,
 			},
-			Body: body,
 		})
 		require.NoError(t, err)
 
@@ -397,20 +410,24 @@ func TestIntegrationEntityServer(t *testing.T) {
 	t.Run("should be able to filter objects based on their labels", func(t *testing.T) {
 		kind := entity.StandardKindDashboard
 		_, err := testCtx.client.Write(ctx, &entity.WriteEntityRequest{
-			GRN: &grn.GRN{
-				ResourceKind:       kind,
-				ResourceIdentifier: "blue-green",
+			Entity: &entity.Entity{
+				GRN: &grn.GRN{
+					ResourceKind:       kind,
+					ResourceIdentifier: "blue-green",
+				},
+				Body: []byte(dashboardWithTagsBlueGreen),
 			},
-			Body: []byte(dashboardWithTagsBlueGreen),
 		})
 		require.NoError(t, err)
 
 		_, err = testCtx.client.Write(ctx, &entity.WriteEntityRequest{
-			GRN: &grn.GRN{
-				ResourceKind:       kind,
-				ResourceIdentifier: "red-green",
+			Entity: &entity.Entity{
+				GRN: &grn.GRN{
+					ResourceKind:       kind,
+					ResourceIdentifier: "red-green",
+				},
+				Body: []byte(dashboardWithTagsRedGreen),
 			},
-			Body: []byte(dashboardWithTagsRedGreen),
 		})
 		require.NoError(t, err)
 